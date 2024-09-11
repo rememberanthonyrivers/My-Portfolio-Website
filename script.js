122a2ee56817a2cdeff9a7254036513daea1048a
Anthony Rivers <81377656+rememberanthonyrivers@users.noreply.github.com>
2024-09-11 09:28:28 -0400

    feat: updated typedJS

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 // typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['Content Creater', 'Fullstack Developer', 'Drone Pilot'],
+    strings: ['Fullstack Developer', 'Drone Pilot'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
